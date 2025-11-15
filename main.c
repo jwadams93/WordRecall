4461f2124a2aac598c3e32a950fa88d9b2a1d2ea
jwadams93 <jwadams93@gmail.com>
2025-11-15 12:03:35 -0500

    add random letter generation and build spinner

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <time.h>
 
 //Linked list
 typedef struct Node {
@@ -36,13 +36,13 @@ int buildWordSearchStructure() {
 
   while (fgets(buffer, sizeof(buffer), file) != NULL) {
 
-      printf("Read: %s\n", buffer);
-      printf("First position of buffer: %c\n", buffer[0]);
+      //printf("Read: %s\n", buffer);
+      //printf("First position of buffer: %c\n", buffer[0]);
       //printf("buffer[0] = '%c' (ASCII: %d)\n", buffer[0], buffer[0]);
 
     if (buffer[0] == currentChar) {
       Node *nextNode = createNode(buffer);
-      printf("Created new node with word: %s in the %d index of the alphabet array\n", nextNode->word, arrPos);
+      //printf("Created new node with word: %s in the %d index of the alphabet array\n", nextNode->word, arrPos);
       temp = nextNode->next;
       alphabet[arrPos] = nextNode;
     } else {
@@ -53,20 +53,48 @@ int buildWordSearchStructure() {
       arrPos = buffer[0] - 'a';
 
       Node *nextNode = createNode(buffer);
-      printf("Created new node with word: %s in the %d index of the alphabet array\n", nextNode->word, arrPos);
+      //printf("Created new node with word: %s in the %d index of the alphabet array\n", nextNode->word, arrPos);
       temp = nextNode->next;
       alphabet[arrPos] = nextNode;
     }
   }
 
   if(feof(file)) {
-    printf("End of file reached.\n");
+    //printf("End of file reached.\n");
   } else if (ferror(file)) {
     printf("An error occured.\n");
   }
   
   fclose(file);
   return 0;
+}
+
+char getRandLetter() {
+  //Could just gen a random number from .. ascii a to ascii z?
+  // then translate that int into a char
+  int lower_bound = 97;
+  int upper_bound = 122;
+
+  // mod the random number to get a number between [0, 26] (rand % (122-97+1 = 26))
+  // then, adding the lower_bound shifts it to [97, 123]
+  return rand() % (upper_bound - lower_bound + 1) + lower_bound;
+
+}
+
+void buildWithSpinner() {
+  char spinChars[] = {'|', '/', '-', '\\'};
+  int spinIdx = 0;
+
+  //hide cursor
+  printf("\033[?25l");
+
+  for (int i = 0; i < 50; i++) {
+    printf("\r%c Building word database... %d%%", spinChars[spinIdx % 4], (i * 100) / 50);
+    fflush(stdout);
+    usleep(90000);
+    spinIdx++;
+  }
+  printf("\r######## Database built! ########\n");
 }
 
 /**
@@ -82,15 +110,16 @@ int buildWordSearchStructure() {
 **/ 
 
 int main() {
-
+  srand(time(NULL));
   //TODO 
   //  Add logic to handle user input 
   //  Add timer that will automatically end the game 
   //    when it runs out of time
   //  Add logic to score player (take each entered word and validate its in the wordSearchStructure)
   //  Add logic display the score (Maybe use cool ascii art thing) and save score to DB file 
-
+  buildWithSpinner();
   buildWordSearchStructure();
-
+  printf("\nYour letter is.....\n");
+  printf("\n%c!\n", getRandLetter());
   return 0;
 }
